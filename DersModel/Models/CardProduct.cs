130b46eec79018dce0040d80ba15127ba384269c
Melis Akay <melisbegumakay@yahoo.com>
2023-08-18 21:12:35 +0300

    Eklemeler yapıldı

--- DersModel/Models/CardProduct.cs
+++ DersModel/Models/CardProduct.cs
@@ -4,5 +4,7 @@
     {
         public int Id { get; set; }
         public Product Product { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
     }
 }
